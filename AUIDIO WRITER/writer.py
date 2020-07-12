fc74993fbce82b32d6d7f16f1c1701c2644b103f
Ambesh Tiwari <35375583+ambesh123@users.noreply.github.com>
2020-07-12 18:23:40 +0530

    Added python codes
    Added python code writer.py to convert speech into string and delegate drawing task to java class NameWriter.java (using Board.java)

---
+++ AUIDIO WRITER/writer.py
@@ -0,0 +1,47 @@
+import os
+import speech_recognition as sr 
+import pyttsx3
+  
+# Initialize the recognizer  
+r = sr.Recognizer()  
+  
+# Function to convert text to 
+# speech 
+def SpeakText(command):    
+    # Initialize the engine 
+    engine = pyttsx3.init() 
+    engine.say(command)  
+    engine.runAndWait() 
+      
+      
+# Loop infinitely for user to 
+# speak 
+  
+while(1):     
+      
+    # Exception handling to handle 
+    # exceptions at the runtime 
+    try: 
+          
+        # use the microphone as source for input. 
+        with sr.Microphone() as source2: 
+              
+            # wait for a second to let the recognizer 
+            # adjust the energy threshold based on 
+            # the surrounding noise level  
+            r.adjust_for_ambient_noise(source2, duration=0.2) 
+              
+            #listens for the user's input  
+            audio2 = r.listen(source2) 
+              
+            listenedText = r.recognize_google(audio2) 
+            listenedText = listenedText.lower() 
+            print("Listened: " + listenedText)
+            os.system('java NameWriter "' + listenedText + '"')
+            SpeakText(listenedText) 
+              
+    except sr.RequestError as e: 
+        print("Could not request results; {0}".format(e)) 
+          
+    except sr.UnknownValueError: 
+        print("unknown error occured") 
